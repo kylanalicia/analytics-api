4d320d49b9d1dac19608f70d9d47602851f2f476
kylanalicia <aliciakimani.ky@gmail.com>
2025-03-30 18:47:20 +0300

    Used pydantic schemas for incoming data validation

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,5 +1,10 @@
 from fastapi import APIRouter
-from .schemas import EventSchema, EventListSchema
+from .schemas import (
+    EventSchema,
+    EventListSchema, 
+    EventCreateSchema,
+    EventUpdateSchema
+)
 
 router = APIRouter()
 
@@ -20,8 +25,8 @@ def read_events() -> EventListSchema:
 # create view
 # POST /api/events/@router.post("/")
 @router.post("/")
-def create_events(data: dict = {}) -> EventSchema:
-    print(type(data))
+def create_events(payload: EventCreateSchema) -> EventSchema:
+    print(type(payload.page))
     return {"id": 123}
 
 
@@ -34,8 +39,9 @@ def get_event(event_id:int) -> EventSchema:
 # Update this data
 # PUT /api/events/12
 @router.put("/{event_id}")
-def update_event(event_id:int, payload:dict={}) -> EventSchema: 
+def update_event(event_id:int, payload:EventUpdateSchema) -> EventSchema: 
     # a single row
+    print(payload.description)
     return{"id": event_id}
 
 # @router.delete("/{event_id}")
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,9 +1,21 @@
 from typing import List
 from pydantic import BaseModel
 
+"""
+id
+page
+description
+"""
+
+
+class EventCreateSchema(BaseModel):
+    page:str
+
+class EventUpdateSchema(BaseModel):
+    description:int
+
 class EventSchema(BaseModel):
     id:int
-
 
 # {"id": 12}
 
