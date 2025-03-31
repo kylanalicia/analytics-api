b80b147ffa9a573d5b329e9b8728442496064a06
kylanalicia <aliciakimani.ky@gmail.com>
2025-03-31 09:45:03 +0300

    created database tables with fastapi lifespan

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
+from contextlib import asynccontextmanager
 from typing import Union
 
 from fastapi import FastAPI
+from api.db.session import init_db
 from src.api.events import router as event_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # before app startup
+    init_db()
+    yield
+    # clean up
+
+app = FastAPI(lifespan=lifespan)
 # /api/events
 app.include_router(event_router, prefix='/api/events')
-
 
 @app.get("/")
 def read_root():
